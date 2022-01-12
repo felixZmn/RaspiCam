228ed995de4b8c12ba2c14c175716a2e4b8c9cad
MHummel <mhummel@artiso.com>
2022-01-12 16:08:35 +0100

    create class from script

--- python-scripts/servo-test.py
+++ python-scripts/servo-test.py
@@ -1,29 +1,32 @@
 import RPi.GPIO as GPIO
 import time
-servoPIN = 18
+# servoPIN = 18
 
-# min 4
-# mid 7.5
-# max 10
-servoPositions = [4, 5, 7.5, 10]
+minValue = 4
+midValue = 7.2
+maxValue = 10
+# servoPositions = [4, 7.2, 10]
 
 
 def setServoCycle(p, position):
     p.ChangeDutyCycle(position)
     # eine Pause von 0,5 Sekunden
-    time.sleep(1)
+    time.sleep(0.5)
 
 
-try:
-    # damit wir den GPIO Pin ueber die Nummer referenzieren koennen
-    GPIO.setmode(GPIO.BCM)
-    # setzen des GPIO Pins als Ausgang
-    GPIO.setup(servoPIN, GPIO.OUT)
-    p = GPIO.PWM(servoPIN, 50)  # GPIO als PWM mit 50Hz
-    pos = 4
-    p.start(pos)
-    setServoCycle(p, pos)
-except KeyboardInterrupt:
-    p.stop()
-    # alle Pins zuruecksetzen
-    GPIO.cleanup()
+class Servo:
+    def __init__(self, servoPIN):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(servoPIN, GPIO.OUT)
+        self.p = GPIO.PWM(servoPIN, 250)
+        self.p.start(midValue)
+
+    def __del__(self):
+        self.p.stop()
+        GPIO.cleanup()
+
+    def move(self, position):
+        # use position range from 0%-100% or -06 to 60
+        if position < minValue or position > maxValue:
+            return
+        setServoCycle(self.p, position)
