b9bb07daa0e2979bda74b8dab9de5c28409a4e12
Samuel Kraut <skraut@artiso.com>
2022-01-28 10:41:27 +0100

    First changes, but i cant run it beacuase I dont want to download the extensions. On Friday
    Da ich die Extension nicht installiert habe, gibt es gerade auch keine Codeüberprüfung

--- python-scripts/servo.py
+++ python-scripts/servo.py
@@ -1,23 +1,34 @@
 from numpy import interp
 import RPi.GPIO as GPIO
+import PCA9685 as servoDriver
 import time
-minValue = 4
-midValue = 7.2
-maxValue = 10
+minValue = 900
+maxValue = 2100
+midValue = minValue + (maxValue -minValue)
+
 
 
 class Servo:
-    def __init__(self, servoPIN):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(servoPIN, GPIO.OUT)
-        self.p = GPIO.PWM(servoPIN, 50)
-        self.p.start(midValue)
+    def __init__(self, servoChannel):
+        self.servoChannel = servoChannel
+        servo = servoDriver(0x40)
+        servo.setPWMFreq(50)
+        servo.setPWm(servoChannel, 0, 1024)
+        servo.setServoPulse(servoChannel, midValue)  # 1500 is centered
+        self.servo = servo
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(servoChannel, GPIO.OUT)
+        #self.p = GPIO.PWM(servoChannel, 50)
+        #self.p.start()
+        
 
     def __del__(self):
-        self.p.stop()
-        GPIO.cleanup()
+        #self.p.stop()
+        #GPIO.cleanup()
+        return 0
 
     def move(self, position):
         value = interp(position, [0, 100], [minValue, maxValue])
-        self.p.ChangeDutyCycle(value)
+        self.servo.setServoPulse(self.servoChannel, value)
+        #self.p.ChangeDutyCycle(value)
         time.sleep(0.1)
